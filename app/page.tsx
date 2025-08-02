4aefeea95c40bcfb4e50fb92927479a17fdad896
asadrehman1 <asadrehman7890661@gmail.com>
2025-08-02 20:21:22 +0500

    Complete Navbar, Guest, About and Contact pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-export default function HomePage() {
-  return (
-    <div>Home</div>
-  )
+import { currentUser } from "@clerk/nextjs/server";
+import Guest from "@/components/Guest";
+export default async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
+  return <div>Home</div>;
 }
