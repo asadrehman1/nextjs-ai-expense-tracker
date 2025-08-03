5f9de3d59a7959ad2cac7c3e4c4ce798b2e18340
asadrehman1 <asadrehman7890661@gmail.com>
2025-08-03 20:51:14 +0500

    Added Expense stats in the dashboard

---
+++ app/actions/getBestWorstExpense.ts
@@ -0,0 +1,39 @@
+"use server";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+
+async function getBestWorstExpense(): Promise<{
+  bestExpense?: number;
+  worstExpense?: number;
+  error?: string;
+}> {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return { error: "User not found" };
+  }
+
+  try {
+    const records = await db.record.findMany({
+      where: { userId },
+      select: { amount: true },
+    });
+
+    if (!records || records.length === 0) {
+      return { bestExpense: 0, worstExpense: 0 };
+    }
+
+    const amounts = records.map((record) => record.amount);
+
+    // Calculate best and worst expense amounts
+    const bestExpense = Math.max(...amounts);
+    const worstExpense = Math.min(...amounts);
+
+    return { bestExpense, worstExpense };
+  } catch (error) {
+    console.error("Error fetching expense amounts:", error);
+    return { error: "Database error" };
+  }
+}
+
+export default getBestWorstExpense;
